573a009754fe4b4472e421fbdf35e03044f306a8
Jean Santos <jeancbsan@gmail.com>
2020-11-06 13:45:25 -0300

    Backend projetct created

---
+++ back-end/src/main/java/com/fourtk/systemcell/SystemcellApplication.java
@@ -0,0 +1,13 @@
+package com.fourtk.systemcell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemcellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemcellApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/fourtk/systemcell/SystemcellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourtk.systemcell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemcellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
